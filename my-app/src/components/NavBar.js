ec8eb0c21388dc35fe27c3d3de2e8535f65b0b39
portialii89@gmail.com <portiali@portias-air.mynetworksettings.com>
2025-01-04 03:33:45 -0500

    styled navbar

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -11,12 +11,15 @@ function NavBar() {
         <nav>
             <ul>
                 <li>
-                    CampusClicks</li>
+                    <span className="bold-c">c</span>ampus<span className="bold-c">c</span>licks
+                </li>
+                {/* <li>
+                    campusclicks</li> */}
                 <li><i class="fa-solid fa-house"></i>
-                    <a>Home</a></li>
-                <li><i class="fa-regular fa-square-plus"></i><a>Create</a></li>
-                <li><i class="fa-solid fa-medal"></i><a>Leaderboard</a></li>
-                <li><i class="fa-solid fa-comment-dots"></i><a>Messages</a></li>
+                    <a>home</a></li>
+                <li><i class="fa-regular fa-square-plus"></i><a>create</a></li>
+                <li><i class="fa-solid fa-medal"></i><a>leaderboard</a></li>
+                <li><i class="fa-solid fa-comment-dots"></i><a>messages</a></li>
                 {/* <li>  <p>Count: {count}</p></li>
                 <li><button onClick={addCount}>Increment</button></li> */}
             </ul>
